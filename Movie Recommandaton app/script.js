6233a9ebb4574e6145008e037cbc1ef24ece9535
Ayoub Unitop <142146489+ayoub717@users.noreply.github.com>
2024-08-31 09:44:35 +0000

    bunch of other projects

---
+++ Movie Recommandaton app/script.js
@@ -0,0 +1,15 @@
+
+
+//the function that will suggest the movies
+function suggest(){
+    // declaring variables
+    const MovieType = document.getElementById('movieType').value;
+    const Year = document.getElementById('year').value;
+    const Country = document.getElementById('country').value;
+    //////////////////////////////////////////////////////////////
+    //fetching API
+    document.querySelector('.response').innerHTML= "Harry Potter";
+    
+
+
+}
